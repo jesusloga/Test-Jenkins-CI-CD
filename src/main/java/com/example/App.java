11b1b754713d32a1768ca6a03d9229066fc643b1
Ubuntu <ubuntu@ip-172-31-20-68.ap-south-1.compute.internal>
2025-09-26 07:33:46 +0000

    check cicd pipline change in app.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "To test CI/CD jenkin-git!" );
+        System.out.println( "To test CI/CD jenkin-git push!" );
     }
 }
